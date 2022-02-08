e2b25707325c75729c750e0641fa45fe743d33b8
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-02-08 20:35:18 +0100

    delete funcionality done

--- services/courses.service.js
+++ services/courses.service.js
@@ -3,9 +3,11 @@ const httpClient = require('./base.service.js');
 const getCourses = () => httpClient.get('/courses');
 const getCourse = (courseId) => httpClient.get(`/courses/${courseId}`);
 const createCourse = (course) => httpClient.post('/courses', course);
+const deleteCourse = (courseId) => httpClient.delete(`/courses/${courseId}`)
 
 module.exports = {
     getCourses,
     getCourse,
-    createCourse
+    createCourse,
+    deleteCourse
 }
